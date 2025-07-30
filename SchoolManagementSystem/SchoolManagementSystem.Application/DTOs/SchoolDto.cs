378ed3741c0c42aa73028ac26c75076250427a5f
Arafat <arafat.developer2008@gmail.com>
2025-07-30 21:55:24 +0600

    DTO Add All

---
+++ SchoolManagementSystem/SchoolManagementSystem.Application/DTOs/SchoolDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagementSystem.Application.DTOs
+{
+    internal class SchoolDto
+    {
+        public string SchoolName { get; set; }
+        public string EiinNumber { get; set; }
+    }
+}
